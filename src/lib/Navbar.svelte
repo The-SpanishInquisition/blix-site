c3d529d1247eb9541e7dd0576973d2ee2c0df18b
Jake Mileham <jake.mileham@gmail.com>
2023-10-26 22:55:20 +0200

    Add github logos and links

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
-    import { AppBar } from "@skeletonlabs/skeleton";
+  import { AppBar } from "@skeletonlabs/skeleton";
 
-    const openGithub = () => {
-        window.open("https://github.com/COS301-SE-2023/AI-Photo-Editor", "_blank");
-    };
-
+  const openGithub = () => {
+    window.open("https://github.com/COS301-SE-2023/AI-Photo-Editor", "_blank");
+  };
 </script>
 
-<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">
-        <img src="blix.svg" alt="Blix logo" class="logo"/>
-    </svelte:fragment>
-    Page under construction. Coming soon!
-	<svelte:fragment slot="trail">
-        <button class="btn btn-primary" on:click={openGithub}>GitHub</button>
-    </svelte:fragment>
+<AppBar
+  gridColumns="grid-cols-3"
+  slotDefault="place-self-center"
+  slotTrail="place-content-end"
+>
+  <svelte:fragment slot="lead">
+    <img src="blix.svg" alt="Blix logo" class="logo" />
+  </svelte:fragment>
+  Page under construction. Coming soon!
+  <svelte:fragment slot="trail">
+    <button class="btn btn-primary" on:click={openGithub}>
+      <img src="github.svg" alt="Github" />
+    </button>
+  </svelte:fragment>
 </AppBar>
 
 <style>
-    .logo {
-        width: 3em;
-    }
+  .logo {
+    width: 3em;
+  }
 </style>
